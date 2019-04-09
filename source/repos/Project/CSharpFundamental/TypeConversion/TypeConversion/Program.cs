248ebe50731ea99c1edc43b4d0ff2adbab5d9dbb
Ardhiana <ardhiananurhakim@yahoo.co.id>
2019-04-09 13:12:08 +0700

    first commit

---
+++ source/repos/Project/CSharpFundamental/TypeConversion/TypeConversion/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+
+namespace TypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string str = "true";
+                bool b = Convert.ToBoolean(str);
+                Console.WriteLine(b);
+
+            }
+
+            catch (Exception)
+            {
+                Console.WriteLine("The Number  Could not be Converted to a byte ");
+            }
+        }
+    }
+}
