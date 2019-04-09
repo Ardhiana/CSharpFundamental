099c8f2ecc76f5b24206dbae92645f277c597b78
Ardhiana <ardhiananurhakim@yahoo.co.id>
2019-04-10 00:05:22 +0700

    first commit

---
+++ source/repos/Project/CSharpFundamental/CSharpFundamentals/CSharpFundamentals/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CSharpFundamentals
+{
+    public class Person
+    {
+        public string FirstName;
+        public string LastName;
+
+        public void introduce()
+        {
+            Console.WriteLine("My Name is " + FirstName, " " + LastName);
+        }
+    }
+}
---
+++ source/repos/Project/CSharpFundamental/CSharpFundamentals/CSharpFundamentals/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CSharpFundamentals
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var john = new Person();
+            john.FirstName = "John";
+            john.LastName = "Smith";
+            john.introduce();
+
+            Calculator calculator = new Calculator();
+            var result = calculator.Add(1, 2);
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+           
+        }
+    }
+}
