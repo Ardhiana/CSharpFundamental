ad76da0eb03a593b4037864f00bf95de0258deee
Ardhiana <ardhiananurhakim@yahoo.co.id>
2019-04-10 11:07:07 +0700

    first commit

---
+++ source/repos/Project/CSharpFundamental/String/String/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstName = "Mosh";
+            var lastName = "Hamdani";
+
+            var fullName = firstName + "" + lastName;
+
+            var myFullName = string.Format("My Name is {0} {1}", firstName, lastName);
+
+            var names = new string[3] { "John", "Marry", "Asep" };
+
+            var formattedNames = string.Join(",", names);
+
+            var text = @"Hi John 
+Look Into the Following paths
+c:\folder1\folder2
+c:\folder3\folder4";
+            Console.WriteLine(text);
+
+            Console.ReadKey();
+        }
+    }
+}
