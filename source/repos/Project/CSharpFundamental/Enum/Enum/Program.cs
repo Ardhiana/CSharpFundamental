0bbc79e181c0d7659371e6d766e0e72989b4ec05
Ardhiana <ardhiananurhakim@yahoo.co.id>
2019-04-10 13:40:10 +0700

    first commit

---
+++ source/repos/Project/CSharpFundamental/Enum/Enum/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum
+{
+
+    public enum ShippingMethod
+    {
+        RegularAirMail = 1,
+        RegisterAirMail = 2,
+        Express = 3,
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var method = ShippingMethod.Express;
+            Console.WriteLine((int)method);
+
+            var methodId = 3;
+            Console.WriteLine((ShippingMethod)methodId);
+
+            Console.WriteLine(method.ToString());
+
+            var methodName = "Express";
+            var shippingMethod = (ShippingMethod) Enum.Parse(typeof (ShippingMethod), methodName);
+
+            Console.ReadKey();
+        }
+    }
+}
