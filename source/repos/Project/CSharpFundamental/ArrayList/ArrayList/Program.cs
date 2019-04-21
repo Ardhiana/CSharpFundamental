aadbdc0af90eca87dfe34449e6e6fe078c3b893b
Ardhiana <ardhiananurhakim@yahoo.co.id>
2019-04-21 22:15:10 +0700

    first commit

---
+++ source/repos/Project/CSharpFundamental/ArrayList/ArrayList/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new[] { 3, 7, 9, 2, 14, 6 };
+
+            Console.WriteLine("Length :" + numbers.Length);
+
+            var index = Array.IndexOf(numbers, 9);
+            Console.WriteLine("Index of 9 : " + index);
+
+            Array.Clear(numbers, 0,2);
+
+            Console.WriteLine("Effect The Clear()");
+            foreach (var n in numbers)
+                Console.WriteLine(n);
+        }
+    }
+}
