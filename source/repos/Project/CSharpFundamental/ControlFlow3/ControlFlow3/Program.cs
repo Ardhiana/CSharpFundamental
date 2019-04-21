788e903633afd9a1e88b593773e66349aa186774
Ardhiana <ardhiananurhakim@yahoo.co.id>
2019-04-21 22:11:49 +0700

    first commit

---
+++ source/repos/Project/CSharpFundamental/ControlFlow3/ControlFlow3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlFlow3
+{
+    partial class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            var season = Season.Autumn;
+
+            switch (season)
+            {
+                case Season.Autumn:
+                    Console.WriteLine("Aku Pulang");
+                    break;
+
+                case Season.Summer:
+                    Console.WriteLine("Pulang Ke Jonggol");
+                    break;
+
+                default:
+                    Console.WriteLine("Aku Pulang");
+                    break;
+            }
+        }
+
+
+    }
+}
---
+++ source/repos/Project/CSharpFundamental/ControlFlow3/ControlFlow3/Season.cs
@@ -0,0 +1,15 @@
+﻿namespace ControlFlow3
+{
+    partial class Program
+    {
+        public enum Season
+        {
+            Spring,
+            Summer,
+            Autumn,
+            Winter,
+        }
+
+
+    }
+}
